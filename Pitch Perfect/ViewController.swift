07787628f8978416f7cde3302a48f632f75dbf25
SCrabtree <sean.crabtree@gmail.com>
2015-04-07 07:49:00 -0700

    Stop Button. Lifecycle.  Navigation Controller.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        recordButton.enabled = true
+        stopButton.hidden = true
+    }
+
+    @IBAction func recordAudio(sender: AnyObject) {
+        recordButton.enabled = false
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        
+        //TODO: Record the user's voice
+        println("in recordAudio")
+    }
 
 }
 
